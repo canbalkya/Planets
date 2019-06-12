494c2a8f3085abb59c749a6d0da54e3b6ffdaa3e
Can <kralciplak9@gmail.com>
2019-06-12 10:35:56 +0300

    Developed Entry Parts

---
+++ Planets/View Controllers/EntryVC.swift
@@ -0,0 +1,16 @@
+//
+//  EntryVC.swift
+//  Planets
+//
+//  Created by Can Balkaya on 6/12/19.
+//  Copyright © 2019 Can Balkaya. All rights reserved.
+//
+
+import UIKit
+
+class EntryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
